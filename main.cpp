8a86d657ff8762d4fbf3fc76623314b321564150
alicia-barre <alicia.barre2005@gmail.com>
2024-09-13 09:59:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,12 +17,47 @@ int initMCP9808() {
     return fd;
 }
 
-void main(){
+int ReadTemp(int fd){
+    int temprequest = wiringPiI2CReadReg16(fd, MCP9808_REG_AMBIENT_TEMP);
+    
+    if (temprequest == -1){
+        std::cerr << "Error read temp from sensor.\n";
+        return -1;
+    }
+    
+    
+    return temprequest;
+}
+    
+    
+    
+    
+    
+int main(){
     if (initMCP9808() == -1){
 
-        std::cout << " Error I2C bus inithilision\n"
-        return 0;
+        std::cout << " Error I2C bus inithilision\n";
+        return -1;
+    }else{
+    
+        std::cout << " work I2C bus good \n";
+    
     }
-
+    
+    int fd = initMCP9808();
+    if (fd == -1){
+        std::cerr << "Error initMCP9808\n";
+        return -1;
+    }
+    int temp = ReadTemp(fd);
+    
+    std::cout << temp << "\n";
+    int data = ((temp & 0xff) << 8 | (temp & 0xff00) >> 8);
+    std::cout << data << "\n";
+    data = (data & 0x0fff);
+    std::cout << data << "\n";
+    data = (data / 16.0);
+    std::cout << data << "\n";
+    return 0;
 
 }
