03b1166d6b465d408f3b1929aae80e95e222712b
Eden <94796854+edenbwt@users.noreply.github.com>
2024-09-15 09:44:29 +0200

    Update sensor.h

--- sensor.h
+++ sensor.h
@@ -1,27 +1,62 @@
+/*****************************************************************//**
+ * \file   sensor.h
+ * \brief  Déclaration de la classe MCP9808Sensor
+ *
+ * Ce fichier contient la déclaration de la classe MCP9808Sensor 
+ * utilisée pour interagir avec un capteur de température MCP9808 via 
+ * une interface I2C.
+ *
+ * \author Dioxigene
+ * \date   Septembre 2024
+ *********************************************************************/
+
 #ifndef SENSOR_H
 #define SENSOR_H
 
+/**
+ * \class MCP9808Sensor
+ * \brief Classe permettant l'interaction avec le capteur MCP9808.
+ * 
+ * Cette classe permet d'initialiser le capteur, de lire la température
+ * et d'afficher les résultats sur la sortie standard.
+ */
 class MCP9808Sensor {
-private:
-    int address;
-    int fd;
-    float temperature;
-
 public:
-    // Constructor to initialize the sensor with an I2C address
+    /**
+     * \brief Constructeur de la classe MCP9808Sensor.
+     * 
+     * \param addr Adresse I2C du capteur MCP9808.
+     */
     MCP9808Sensor(int addr);
 
-    // Method to initialize I2C communication
+    /**
+     * \brief Initialise la communication I2C avec le capteur.
+     * 
+     * \return true si l'initialisation est réussie, false sinon.
+     */
     bool init();
 
-    // Method to read temperature from the sensor
+    /**
+     * \brief Récupère la température actuelle du capteur.
+     * 
+     * \return La température en degrés Celsius, ou -1 en cas d'erreur.
+     */
     float getTemperature();
 
-    // Method to display the current temperature
+    /**
+     * \brief Affiche la température actuelle sur la console.
+     */
     void displayTemperature();
 
-    // Destructor (optional)
+    /**
+     * \brief Destructeur de la classe MCP9808Sensor.
+     */
     ~MCP9808Sensor();
+
+private:
+    int address;      ///< Adresse I2C du capteur MCP9808
+    int fd;           ///< Descripteur de fichier pour la communication I2C
+    float temperature;///< Dernière température lue en degrés Celsius
 };
 
-#endif
+#endif // SENSOR_H
